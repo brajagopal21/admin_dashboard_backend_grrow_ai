51fd7200317d17d6db03c6f4813a049e1875d7ba
Fardeen Ahamed <1si20et403@sit.ac.in>
2024-03-23 16:34:44 +0530

    encryption added for signup

--- Controllers/signup-controller.js
+++ Controllers/signup-controller.js
@@ -1,4 +1,6 @@
 const { default: mongoose } = require("mongoose");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 const User = require("../Models/User-model");
 const { configDotenv } = require("dotenv");
 configDotenv();
@@ -6,10 +8,27 @@ const signupUser = async (req, res) => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     const { name, email, password } = req.body;
-    const newUser = new User({ name, email, password, isAdmin: false });
+    const ciphertext = CryptoJS.AES.encrypt(
+      password,
+      `${process.env.SECRET_KEY}`
+    ).toString();
+    const newUser = new User({
+      name,
+      email,
+      password: ciphertext,
+      isAdmin: false,
+    });
     await newUser.save();
+    const token = jwt.sign(
+      { success: true, email: email, name: name },
+      `${process.env.SECRET_KEY}`,
+      { expiresIn: "1d" }
+    );
+
     mongoose.disconnect();
-    return res.status(200).json({ message: "user created successfully" });
+    return res
+      .status(200)
+      .json({ token: token, message: "user created successfully" });
   } catch (error) {
     console.error(error);
   }
